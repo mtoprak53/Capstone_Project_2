808b3d7a62fb7f4d73accca660653a9a856bf468
mete <25986959+mtoprak53@users.noreply.github.com>
2024-02-24 03:21:36 -0500

    GH Pages trial 2

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   // base: "/",
-  base: "/Capstone_Project_2/frontend",
+  base: "/Capstone_Project_2/frontend/src",
   plugins: [react()],
   // preview: {
   //  port: 8080,
