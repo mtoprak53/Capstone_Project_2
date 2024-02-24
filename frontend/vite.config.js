4cb411234d6766e17e3546e120dc10d4aaa08374
mete <25986959+mtoprak53@users.noreply.github.com>
2024-02-24 04:45:31 -0500

    Vercel trial

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   // base: "/",
-  base: "/Capstone_Project_2/frontend/src",
+  // base: "/Capstone_Project_2",
   plugins: [react()],
   // preview: {
   //  port: 8080,
